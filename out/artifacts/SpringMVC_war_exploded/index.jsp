98e156e59a9125c825b62c3392ca4e0b0d72381e
songsunkook <songsunkook@gmail.com>
2020-09-29 02:13:51 +0900

    .

---
+++ out/artifacts/SpringMVC_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: songsunkook
+  Date: 2020-09-29
+  Time: 오전 1:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+    This is index page
+  </body>
+</html>
---
+++ src/main/java/controller/TestController.java
@@ -0,0 +1,21 @@
+package controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @RequestMapping(value = "/hellotest1", method = RequestMethod.GET)
+    public String helloTest1(){
+        return "hello";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/hellotest2", method = RequestMethod.GET)
+    public String helloTest2(){
+        return "hello";
+    }
+}
